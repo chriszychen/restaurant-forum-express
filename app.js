5d0737d73ec373a3ad3b7c26d8af10dcab4dca26
Chris Chen <ok3201123@gmail.com>
2021-06-16 17:19:45 +0800

    feat: add signup validation and error flash messages

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models')
 const app = express()
 const port = 3000
 
 app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync()
