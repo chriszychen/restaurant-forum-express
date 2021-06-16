c6ac2d35af0bf94b761b3ad7be721f47ee09fa27
Chris Chen <ok3201123@gmail.com>
2021-06-16 16:55:58 +0800

    feat: install bcrypt and add signup page

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const port = 3000
 app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.listen(port, () => {
   db.sequelize.sync()
   console.log(`Example app listening at http://localhost:${port}`)
