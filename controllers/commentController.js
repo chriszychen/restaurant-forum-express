183985f50644bcfd39e25a73c5585c6c3487aa61
Chris Chen <ok3201123@gmail.com>
2021-06-23 20:16:03 +0800

    feat: add delete comment feature for admin

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -9,6 +9,13 @@ const commentController = {
       UserId: req.user.id
     })
       .then(comment => res.redirect(`/restaurants/${req.body.restaurantId}`))
+  },
+  deleteComment: (req, res) => {
+    return Comment.findByPk(req.params.id)
+      .then(comment => {
+        return comment.destroy()
+          .then(comment => res.redirect(`/restaurants/${comment.RestaurantId}`))
+      })
   }
 }
 
