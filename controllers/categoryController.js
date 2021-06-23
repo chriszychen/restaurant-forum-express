e8367c3e9ee080490f507c07e1488ef9ebcfbc14
Chris Chen <ok3201123@gmail.com>
2021-06-23 10:34:34 +0800

    feat: add admin categories post feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -7,6 +7,15 @@ const categoryController = {
       .then(categories => {
         res.render('admin/categories', { categories })
       })
+  },
+  postCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', 'name didn\'t exist')
+      return res.redirect('back')
+    } else {
+      return Category.create({ name: req.body.name })
+        .then(category => res.redirect('/admin/categories'))
+    }
   }
 }
 
