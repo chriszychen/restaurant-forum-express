28dc681f09db2c8f583511688657268f43a1cb86
Chris Chen <ok3201123@gmail.com>
2021-06-24 17:41:53 +0800

    feat: add update user profile feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
+const fs = require('fs')
+const helpers = require('../_helpers.js')
 
 const userController = {
   signUpPage: (req, res) => {
@@ -30,6 +32,7 @@ const userController = {
         .catch(err => console.log(err))
     }
   },
+
   signInPage: (req, res) => {
     return res.render('signin')
   },
@@ -43,6 +46,65 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+
+  getUser: (req, res) => {
+    const sameUser = helpers.getUser(req).id === Number(req.params.id)
+    return User.findByPk(req.params.id)
+      .then(user => res.render('user', { user: user.toJSON(), sameUser: sameUser }))
+  },
+
+  editUser: (req, res) => {
+    if (helpers.getUser(req).id !== Number(req.params.id)) {
+      req.flash('error_messages', 'you have no authority to edit this user\'s profile')
+      return res.redirect(`/users/${req.params.id}`)
+    }
+    return User.findByPk(req.params.id)
+      .then(user => res.render('editUser', { user: user.toJSON() }))
+  },
+
+  putUser: (req, res) => {
+    if (helpers.getUser(req).id !== Number(req.params.id)) {
+      req.flash('error_messages', 'you have no authority to edit this user\'s profile')
+      return res.redirect('back')
+    }
+
+    if (!req.body.name) {
+      req.flash('error_messages', 'name is required')
+      return res.redirect('back')
+    }
+
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          return User.findByPk(req.params.id)
+            .then(user => {
+              user.update({
+                name: req.body.name,
+                image: `/upload/${file.originalname}`
+              })
+            })
+            .then(user => {
+              req.flash('success_messages', 'user profile was successfully updated')
+              res.redirect(`/users/${req.params.id}`)
+            })
+        })
+      })
+    } else {
+      return User.findByPk(req.params.id)
+        .then(user => {
+          user.update({
+            name: req.body.name,
+            image: '/upload/user-icon.png'
+          })
+        })
+        .then(user => {
+          req.flash('success_messages', 'user profile was successfully updated')
+          res.redirect(`/users/${req.params.id}`)
+        })
+    }
   }
 }
 
