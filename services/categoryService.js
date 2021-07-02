cfad4d9319fee0764b21207dd697d8e0bee7413b
Chris Chen <ok3201123@gmail.com>
2021-07-03 01:22:29 +0800

    feat: add delete category api and refactor categoryController.deleteCategory

--- services/categoryService.js
+++ services/categoryService.js
@@ -37,6 +37,15 @@ const categoryService = {
             })
         })
     }
+  },
+  deleteCategory: (req, res, callback) => {
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        category.destroy()
+          .then(category => {
+            return callback({ status: 'success', message: '' })
+          })
+      })
   }
 }
 
